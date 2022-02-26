e5814f9a68005e3d2ff4a1bc6d4a6b69f12267a5
Brahim Amghough <bamghoug@e1r12p1.1337.ma>
2022-02-26 13:24:14 +0100

    nothing

--- main.cpp
+++ main.cpp
@@ -5,22 +5,14 @@
 
 int main()
 {
-
-std::vector<std::string> v(10, "range constructor test");
-        ft::vector<std::string> my_v(10, "range constructor test");
-        ft::vector<std::string> my_v1(my_v.begin(), my_v.end()); // this one is to check if the range works with ft::vector
-        /*----------------------------------------------------------------------------------------------*/
-        /*--------------- declare tow strings to store the results ------*/
-        std::string res, my_res, my_res1;
-        /*--------------------------------------------------------*/
-        for (std::vector<std::string>::iterator it = v.begin(); it != v.end(); ++it) // fill res from std::vector
-            res += *it;
-
-        for (ft::vector<std::string>::iterator it = my_v.begin(); it != my_v.end(); ++it) // fill my_res from ft::vector
-            my_res += *it;
-
-        for (ft::vector<std::string>::iterator it = my_v1.begin(); it != my_v1.end(); ++it) // fill my_res1 from ft::vector
-            my_res1 += *it;
-        if(res == my_res && my_res == my_res1)
-			std::cout << "true" << std::endl;
+	std::vector<int> v(3, 4);
+    std::reverse_iterator<std::vector<int>::iterator> rit(v.end()), rit_1(v.end() - 1);
+    /*----------------------------------*/
+    /*------------ ft::reverse_iterator ---------*/
+    ft::reverse_iterator<std::vector<int>::iterator> my_rit(v.end()), my_rit1(v.end() - 1);
+	my_rit -= 1;
+    rit -= 1;
+	std::cout << &(*rit) << " && " << &(*(rit_1 - 1)) << std::endl;
+    if((&(*my_rit) == &(*(my_rit1 - 1))) && (&(*rit) == &(*(rit_1 - 1))))
+		std::cout << "true" << std::endl;
 }
